4ba2d7b7ef32bcf9a48b12a4c78741085a0f2367
mattiauba04 <100764100+mattiauba04@users.noreply.github.com>
2023-05-17 19:51:15 +0200

    Update ServerSorcio.js

--- ServerSorcio.js
+++ ServerSorcio.js
@@ -18,10 +18,14 @@ wss.on('connection', (ws) => {
     console.log('Messaggio ricevuto:', message);
     switch (message.type) {
       case 'join-game':
+        console.log("ciao");
         joinGame(message.name, ws);
         break;
      case 'start-game':
         startGame();
+        break;
+    case 'reset-game':
+          resetGame();
         break;
      case 'stop-game':
         stopGame();
@@ -44,6 +48,7 @@ wss.on('connection', (ws) => {
       players.delete(playerId);
       score.delete(playerId);
       console.log('Il giocatore', playerId, 'ha lasciato il gioco');
+      ws.terminate();
     }
   });
 });
@@ -83,43 +88,77 @@ function startGame() {
     gameTimer = setInterval(() => {
       broadcast({ type: 'game-tick', time: 20 - tempo });
       generateMole();
-    }, 1000);    
+    }, 700);    
   }
 }
 
 function generateMole(ws) {
-  moleX = Math.round(Math.random() * 700);
-  moleY = Math.round(Math.random() * 500);
-  broadcast({ type: 'generate', x: moleX, y: moleY });
-  tempo++;
+
+   if (tempo >= 20) {
+      endGame();  
+    }
+  else
+    {
+      moleX = Math.round(Math.random() * 700);
+      moleY = Math.round(Math.random() * 500);
+      broadcast({ type: 'generate', x: moleX, y: moleY });
+      tempo++;
+      
+    }
 }
 
 function hitMole(name) {
   const playerWs = players.get(name);
   if (playerWs) {
     console.log('Il giocatore', name, 'ha colpito la talpa!');
-    score.set(name, (score.get(name) || 0) + 1);
+    score.set(name, (parseInt(score.get(name)) || 0) + 1);
     console.log(score);
     if (tempo >= 20) {
-      endGame();
+      endGame();  
     }
   }
 }
+
+function resetGame() {
+  gameStarted = false;
+  clearTimeout(moleTimer);
+  clearInterval(gameTimer);
+  players.clear();
+  score.clear();
+  tempo = 0;
+  moleX = Math.round(Math.random() * 700);
+  moleY = Math.round(Math.random() * 500);
+
+  // Invia un messaggio di reset a tutti i client
+  broadcast({ type: 'reset-game', x: moleX, y: moleY });
+
+}
+
 
 function endGame() {
   console.log('Fine del gioco!');
   gameStarted = false;
   clearTimeout(moleTimer);
   clearInterval(gameTimer);
-  const scores = Array.from
-(score.entries());
-scores.sort((a, b) => b[1] - a[1]);
-broadcast({ type: 'game-over', scores });
-score.clear();
+  const scores = Array.from(score.entries());
+
+  scores.sort((a, b) => b[1] - a[1]); // Ordina in base al punteggio (dal più alto al più basso)
+
+  // Invia i punteggi aggiornati a tutti i giocatori
+  scores.forEach(([playerName, playerScore]) => {
+    const scoreData = {
+      type: 'game-over',
+      name: playerName,
+      score: playerScore
+    };
+    broadcast(scoreData);
+  });
+
+  score.clear();
 }
 
 function broadcast(message) {
-  const data = JSON.stringify(message);
+  const data = JSON.stringify(message); 
   for (const playerWs of players.values()) {
     playerWs.send(data);
   }
